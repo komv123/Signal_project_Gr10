87b3873f5d8aa2c6da60e7070ad0585d899911f9
komv123 <oriasvas@gmail.com>
2023-09-28 15:37:12 +0200

    Extended main

--- main.m
+++ main.m
@@ -2,9 +2,21 @@
 % All functions shall be put into separate function file for better
 % transparency
 
+clear all
+close all
+clc
+
 % Opening specification file
 specs = specsOpen('global_specs.txt');
 
+if strcmp(specs{1}, 'wav')
+    [y, fs_y] = audioread(specs{2});
+elseif strcmp(specs{1}, 'generate')
+    %Comming soon
+end
+
+
+    
 
 
 
