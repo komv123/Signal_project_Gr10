3bd60b2490c1d5caa79f4d99ef5134b290ee22a6
komv123 <oriasvas@gmail.com>
2023-10-04 14:57:55 +0200

    Error handling

--- main.m
+++ main.m
@@ -10,9 +10,31 @@ clc
 specs = specsOpen('global_specs.txt');
 
 if strcmp(specs{1}, 'wav')
+    %Opening wav file
     [y, fs_y] = audioread(specs{2});
 elseif strcmp(specs{1}, 'generate')
-    %Comming soon
+    %Choosing signal generator
+
+    if strcmp(specs{4}, 'rect')
+        %Rectangle generator (samp_freq(Hz), duration(s), period(s))
+    elseif strcmp(specs{4}, 'tone')
+        %Tone generator (samp_freq(Hz), duration(s))
+    elseif strcmp(specs{4}, 'tone-complex')
+        %Complex-tone generator (samp_freq(Hz), duration(s))
+    elseif strcmp(specs{4}, 'noise')
+        %Noise generator (samp_freq(Hz), duration(s))
+    else
+        error('SignalGenError:WrongType', ...
+            ['Error while choosing signal type.\n' ...
+            'Possible: rect, tone, tone-complex, noise\n' ...
+            'Found: %s'], specs{4})
+    end
+else
+    %error('ErrorTests:convertTest', ...
+      %'In this case, the newline \n is converted.')
+    error('InputError:WrongType', ...
+        ['Error while choosing input signal.\nPossible: wav, generate\n' ...
+        'Found: %s'], specs{1})
 end
 
 
