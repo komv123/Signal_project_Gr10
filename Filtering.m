1f0b6fb2b2b1a27f5c21a4363dfe1de2a082219d
LouisCuendet <louis.cuendet@epfl.ch>
2023-11-06 11:47:44 +0100

    lol

--- Filtering.m
+++ Filtering.m
@@ -5,15 +5,16 @@ clc
 
 freq=[1000 3000];
 fs=20000;
-order=4;
-type='bp';
+order=48;
+type='hp';
 approx = 'butterworth';
 
-[B,A]=Filtergenerator(approx,order,type,freq,fs);
-[h,t]=impz(B,A,1e6);
-plot(t,h)
+[B,A]=IIRFiltergenerator(approx,order,type,freq,fs);
+%[h,t]=impz(B,A,1e6);
+%plot(t,h)
 
-
+b=FIRFiltergenerator(order,type,freq,fs);
+freqz(b,1,[],fs)
 
 
 
--- IIRFiltergenerator.m
+++ IIRFiltergenerator.m
@@ -1,22 +1,22 @@
 function [B,A]=IIRFiltergenerator(approx,order,type,freq,fs)
 
        if approx(1,1:5) == 'cauer'
-           [B,A]=Cauer(order,freq,fs,type)
+           [B,A]=Cauer(order,freq,fs,type);
            return
        end
 
        if approx(1,1:10) == 'chebyshevI'
-           [B,A]=ChebyshevI(order,freq,fs,type)
+           [B,A]=ChebyshevI(order,freq,fs,type);
            return
        end
 
        if approx == 'butterworth'
-           [B,A]=Butterworth(order,freq,fs,type)
+           [B,A]=Butterworth(order,freq,fs,type);
            return
        end
 
        if approx == 'chebyshevII'
-           [B,A]=ChebyshevII(order,freq,fs,type)
+           [B,A]=ChebyshevII(order,freq,fs,type);
            return
        end
 
