e977be4aab8394f0829e4595953ab3e1b0146288
LouisCuendet <louis.cuendet@epfl.ch>
2023-11-16 12:24:42 +0100

    lol

--- main.m
+++ main.m
@@ -6,63 +6,56 @@ close all
 clc
 
 % Opening specification file
-specs = specsOpen('global_specs.txt');
+specs=specOpenV2('global_specs.txt');
 
-if strcmp(specs{1}, 'wav')
+if strcmp(specs.task_type, 'wav')
     %Opening wav file
-    [y, fs_y] = audioread(specs{2});
-elseif strcmp(specs{1}, 'generate')
+    [signal, fs_y] = audioread(specs.wav_filename);
+elseif strcmp(specs.task_type, 'generate')
     %Choosing signal generator
 
-    if strcmp(specs{4}, 'rect')
+    if strcmp(specs.signal_type, 'rect')
         %Rectangle generator (samp_freq(Hz), periodicity(s), duration(s))
-        signal = generate_square(str2double(specs{5}), str2double(specs{7}), str2double(specs{6}));
-    elseif strcmp(specs{4}, 'tone')
+        signal = generate_square(specs.fs, specs.periodicity_s, specs.signal_duration_s);
+    elseif strcmp(specs.signal_type, 'tone')
         %Tone generator (samp_freq(Hz), freq(Hz), duration(s))
-        signal = generate_tone(str2double(specs{5}), str2double(specs{8}), str2double(specs{6}));
-    elseif strcmp(specs{4}, 'tone-complex')
+        signal = generate_tone(specs.fs, specs.frequencies(1), specs.signal_duration_s);
+    elseif strcmp(specs.signal_type, 'tone-complex')
         %Complex-tone generator (samp_freq(Hz), freq(Hz) duration(s))
-        for i = 8:100
-            if strcmp(specs{i}, 'END')
-                break
-            end
-            freqs(i-7) = str2double(specs{i});
-        end
-        signal = generate_tone_complex(str2double(specs{5}), freqs, str2double(specs{6}));
-    elseif strcmp(specs{4}, 'noise')
+        signal = generate_tone_complex(specs.fs, specs.frequencies, specs.signal_duration_s);
+    elseif strcmp(specs.signal_type, 'noise')
         %Noise generator (samp_freq(Hz), duration(s))
-        signal = generate_noise(str2double(specs{5}), str2double(specs{6}));
+        signal = generate_noise(specs.fs, specs.signal_duration_s);
     else
         error('SignalGenError:WrongType', ...
             ['Error while choosing signal type.\n' ...
             'Possible: rect, tone, tone-complex, noise\n' ...
-            'Found: %s'], specs{4})
+            'Found: %s'], specs.signal_type)
     end
 else
     %error('ErrorTests:convertTest', ...
       %'In this case, the newline \n is converted.')
     error('InputError:WrongType', ...
         ['Error while choosing input signal.\nPossible: wav, generate\n' ...
-        'Found: %s'], specs{1})
+        'Found: %s'], specs.task_type)
 end
 
 
 %FILTERING AND PLOTTING OF INPUT SIGNAL AND FILTERED SIGNAL
 
 
-tvect = (1:length(signal))/str2double(specs{5});
+tvect = (1:length(signal))/specs.fs;
 
-specs=specOpenV2('global_specs.txt');
 filtered_signal=Filtering(signal,specs);
 
 figure('Name', "test signal")
-plot(tvect,signal)
+plot(tvect,signal(:,1))
 hold on
 tvect2=(1:length(filtered_signal))/specs.fs;
 plot(tvect,filtered_signal(1:length(signal)))
 
 
-xlim([tvect(1000) tvect(1300)])
+xlim([tvect(1000) tvect(2000)])
 
 
 
